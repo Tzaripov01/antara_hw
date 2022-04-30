8fc21b1a2e8f3bc84abcfab747c078a5c6ee8327
Timur <timur-zaripov01@mail.ru>
2022-04-30 18:42:04 +0300

    main

---
+++ hw1/src/main/java/model/Kotik.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Kotik {
+    private void play(String name)
+    {
+        ;
+    }
+    private void sleep(String name)
+    {
+        ;
+    }
+    private void chaseMouse(String name)
+    {
+        ;
+    }
+    private void eat(String name){
+        ;
+    }
+    private void cuddle(String name){
+        ;
+    }
+}
